53f635a6f6e26a649dba596a3c63cc6c7bc8ac5a
Alex Marginean <alexmarginean@192-168-0-102.rdsnet.ro>
2022-08-04 15:11:02 +0300

    Fixed CORS issue

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, request
 import os
 from dotenv import load_dotenv
 import shopify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 load_dotenv()
 
